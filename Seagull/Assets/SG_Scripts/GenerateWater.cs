5ffed9395ae92737169f48ccbca4934ea65df7fb
RubenTipparach <santipab.stephane@gmail.com>
2016-04-26 12:03:04 -0500

    Check in new files.

---
+++ Seagull/Assets/SG_Scripts/GenerateWater.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerateWater : MonoBehaviour {
+
+	[SerializeField]
+	GameObject water;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 10; i ++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				GameObject o  = (GameObject)GameObject.Instantiate(water, 
+					transform.position + Vector3.left * 100 * i + Vector3.forward * 100* j, 
+					transform.rotation);
+				o.transform.parent = this.transform;
+				
+			}
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
