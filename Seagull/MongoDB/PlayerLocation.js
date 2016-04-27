c57cf69d42b6ae8d146ed8c7b9ce64adb5d56c54
Ruben <Ruben@THE_SEVENTH>
2016-04-27 13:44:42 -0500

    Checking in SQL/Mongo changes.

---
+++ Seagull/MongoDB/PlayerLocation.js
@@ -0,0 +1,21 @@
+/* 1 */
+{
+    "_id" : ObjectId("57210061815a3c22e511e1a3"),
+    "user_Id" : ObjectId("572064afb773ea3328a4ffaf"),
+    "location" : {
+        "x" : 50,
+        "y" : 100,
+        "z" : 50
+    }
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5721011c815a3c22e511e1a4"),
+    "user_id" : ObjectId("5720657cb773ea3328a4ffb0"),
+    "location" : {
+        "x" : 50,
+        "y" : 100,
+        "z" : 50
+    }
+}
