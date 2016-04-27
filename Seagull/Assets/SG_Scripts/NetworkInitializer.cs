6c0b03c37f88639a04c3fe83cfab8d23c1932028
RubenTipparach <santipab.stephane@gmail.com>
2016-04-27 02:38:11 -0500

    Holycrap, a mongo db connection! wowowowowowow

--- Seagull/Assets/SG_Scripts/NetworkInitializer.cs
+++ Seagull/Assets/SG_Scripts/NetworkInitializer.cs
@@ -1,11 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 public class NetworkInitializer : MonoBehaviour {
 
+	public class Users
+	{
+		public ObjectId Id { get; set; }
+		public int user_id { get; set; }
+		public string name { get; set; }
+		public int age { get; set; }
+	}
+
+	private MongoClient _client;
+	private MongoServer _server;
+	private MongoDatabase _db;
+	private MongoCollection<Users> _users;
+
 	// Use this for initialization
 	void Start () {
-	
+		_client = new MongoClient(new MongoUrl("mongodb://localhost")); //27017
+		_server = _client.GetServer();
+
+		_server.Connect();
+		_db = _server.GetDatabase("seagulldb");
+		_users = _db.GetCollection<Users>("Users");
+
+		Debug.Log(string.Format("Found {0} documents." , _users.Count()));
+		var filter = new Users();
+
+		var jsonString = _users.FindAll().ToJson();
+		var cursor = _users.FindAll();
+		Debug.Log(jsonString + "!");
+		foreach (var u in cursor)
+		{
+			Debug.Log(u.name);
+		}
 	}
 	
 	// Update is called once per frame
