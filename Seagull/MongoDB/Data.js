1e7f8345f9a97fa429a7020dba873eab503808ad
RubenTipparach <santipab.stephane@gmail.com>
2016-04-27 02:46:33 -0500

    Adding mongodb files and such.

---
+++ Seagull/MongoDB/Data.js
@@ -0,0 +1,19 @@
+/*Create DB Seagull*/
+
+/*Create Collections*/
+
+/* 1 */
+{
+    "_id" : ObjectId("572064afb773ea3328a4ffaf"),
+    "user_id" : 1,
+    "name" : "Ruben",
+    "age" : 26
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5720657cb773ea3328a4ffb0"),
+    "user_id" : 2,
+    "name" : "Anakin",
+    "age" : 4
+}
