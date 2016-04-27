7e60451db86215a78fc59e8aa250693fbe67896b
Ruben <Ruben@THE_SEVENTH>
2016-04-26 22:18:06 -0500

    Checking in new seagull stuff!

--- Seagull/Assets/SG_Scripts/Bird.cs
+++ Seagull/Assets/SG_Scripts/Bird.cs
@@ -3,13 +3,89 @@ using System.Collections;
 
 public class Bird : MonoBehaviour {
 
+    [SerializeField]
+    Animator animator;
+
+    public Animator Anim
+    {
+        get
+        {
+            return animator;
+        }
+    }
+
+    Rigidbody rigidBody;
+
 	// Use this for initialization
 	void Start () {
-	
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        bool landed = true;
+
+        RaycastHit hit;
+        if(Physics.Raycast(new Ray(transform.position, -transform.up), out hit))
+        {
+            //Debug.Log(hit.distance);
+            if(hit.distance > 1)
+            {
+                landed = false;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Space) && landed)
+        {
+            animator.Play("TakeOff");
+            animator.SetFloat("TakeOff", 1);
+            rigidBody.AddForce(transform.up * Time.deltaTime * 1000);
+        }
+        else if (Input.GetKey(KeyCode.Space) && !landed)
+        {
+            animator.Play("Flap");
+            animator.SetFloat("Flap", 1);
+            rigidBody.AddForce(transform.up * Time.deltaTime * 1000);
+        }
+        else
+        {
+            if (landed)
+            {
+                //animator.SetFloat("TakeOff", 0);
+                bool isIdle = true;
+
+                if (Input.GetKey(KeyCode.W))
+                {
+                    animator.Play("Walk");
+                    animator.SetFloat("Walk", 1);
+                    rigidBody.AddForce(transform.forward * Time.deltaTime * 1000);
+                    isIdle = false;
+                }
+
+                if (isIdle)
+                {
+                    animator.SetFloat("Walk", 0);
+                    animator.Play("Idle");
+                }
+            }
+            else
+            {
+                if (Input.GetKey(KeyCode.W))
+                {
+                    rigidBody.AddForce(transform.forward * Time.deltaTime * 1000);
+                }
+
+                //Debug.Log("We're flying!");
+            }
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * 100 * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * 100 * Time.deltaTime);
+        }
+    }
 }
