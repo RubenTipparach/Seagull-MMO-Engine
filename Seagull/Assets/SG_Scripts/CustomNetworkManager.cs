c398bf540e8f4ec99fb9d8305684d6b8dd9224a7
RubenTipparach <santipab.stephane@gmail.com>
2016-05-05 01:47:16 -0500

    Added dump files and custom network files.

---
+++ Seagull/Assets/SG_Scripts/CustomNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class CustomNetworkManager : NetworkManager
+{
+
+	[SerializeField]
+	Transform playerSpawnLocation;
+
+	// called when a new player is added for a client
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		AddPlayerMDB(conn, playerControllerId);
+    }
+
+	// Add the player using MongoDB
+	private void AddPlayerMDB(NetworkConnection conn, short playerControllerId)
+	{
+		NetworkInitializer initializerScript = GetComponent<NetworkInitializer>();
+		Vector3 spawnPosition = initializerScript.LinkUpPlayerPositionToMDB();
+		var player = (GameObject)GameObject.Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
+		initializerScript.TrackPositionTest = player.transform;
+
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+		// Debug.Log("sdfwefwef");
+	}
+}
